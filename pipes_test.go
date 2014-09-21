5278bec8715fea75a78944db3b4a3b1050906197
Max Sills <sillsm@gmail.com>
2014-09-21 11:17:37 -0700

    Update pipes_test.go

--- pipes_test.go
+++ pipes_test.go
@@ -5,7 +5,18 @@ import (
 )
 
 func TestServerClient(t *testing.T){
-  if true {
-    t.Errorf("Expect to error")
+  MakeAPipe("First")
+  go WriteToPipe("First", []byte("First msg\n"))
+  go WriteToPipe("First", []byte("Second msg\n"))
+  go WriteToPipe("First", []byte("Third msg\n"))
+  
+   
+  b, err := ReadFromPipe("First")
+  if err != nil {
+    t.Errorf("ReadFromPipe(\"First\"): %v ", err)
+  }
+  
+  if string(b) != "First msg\n" {
+    t.Errorf("Want %v, got %v ", "First msg\n", string(b))
   }
 }
