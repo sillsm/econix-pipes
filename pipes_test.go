da413a617553f58b3207fb03bcaead6ffa0ea562
Max Sills <sillsm@gmail.com>
2014-09-21 11:43:43 -0700

    Update pipes_test.go

--- pipes_test.go
+++ pipes_test.go
@@ -6,6 +6,20 @@ import (
 
 func TestServerClient(t *testing.T){
   MakeAPipe("First")
+  
+  var response = make(chan []byte)
+  var answer = make([]byte)
+  
+  go WriteBlockRead("First, []byte("First msg"), response)
+  go WriteToPipe("First", []byte("Second msg\n"))
+  
+  answer <- response
+  if string(answer) != "Second msg" {
+    t.Errorf("Want %v, got %v ", "Second msg", string(answer))
+  }
+  
+  /*
+  
   go WriteToPipe("First", []byte("First msg\n"))
   go WriteToPipe("First", []byte("Second msg\n"))
   go WriteToPipe("First", []byte("Third msg\n"))
@@ -19,4 +33,5 @@ func TestServerClient(t *testing.T){
   if string(b) != "First msg\n" {
     t.Errorf("Want %v, got %v ", "First msg\n", string(b))
   }
+  */
 }
