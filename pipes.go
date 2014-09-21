1ca3539bbbdd0821e256f950597ad0de076e5a75
Max Sills <sillsm@gmail.com>
2014-09-21 10:51:08 -0700

    Create pipes.go

---
+++ pipes.go
@@ -0,0 +1,23 @@
+package pipes
+
+import (
+  "fmt"
+  "log"
+  "syscall"
+  //"os"
+  "io/ioutil"
+)
+
+//By default WriteToPipe blocks execution until there's a reader.
+func WriteToPipe(filename string, msg []byte) error{
+  return ioutil.WriteFile(filename, msg, 0644)
+}
+
+//By default ReadFromPipe blocks execution until there's a writer.
+func ReadFromPipe(filename string)([]byte, error){
+  return ioutil.ReadFile(filename)
+}
+
+func MakeAPipe(name string){
+  syscall.Mknod(name, syscall.S_IFIFO|0666, 0)
+}
