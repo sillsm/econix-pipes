41d23ea57ded07ef89bd1510571c3d4860df1609
Max Sills <sillsm@gmail.com>
2014-09-21 11:25:17 -0700

    Update pipes.go

--- pipes.go
+++ pipes.go
@@ -5,16 +5,26 @@ import (
   "io/ioutil"
 )
 
-//By default WriteToPipe blocks execution until there's a reader.
+// By default WriteToPipe blocks execution until there's a reader.
 func WriteToPipe(filename string, msg []byte) error{
   return ioutil.WriteFile(filename, msg, 0644)
 }
 
-//By default ReadFromPipe blocks execution until there's a writer.
+// By default ReadFromPipe blocks execution until there's a writer.
 func ReadFromPipe(filename string)([]byte, error){
   return ioutil.ReadFile(filename)
 }
 
+// Write to Pipe, Block until content available, Read.
+func WriteBlockRead(filename string, msg []byte)([]byte, error){
+  err := WriteToPipe(filename, msg)
+  if err != nil{
+    return nil, err
+  }
+  return ioutil.ReadFile(filename)
+}
+
+// Make a pipe.
 func MakeAPipe(name string){
   syscall.Mknod(name, syscall.S_IFIFO|0666, 0)
 }
