d85379af6933fcb539ee7b6a436089fb167680b6
Max Sills <sillsm@gmail.com>
2014-10-12 00:43:25 -0400

    Implemented BetterReadBlockWrite- sending and receiving on chans == communicating on named pipes

--- pipes.go
+++ pipes.go
@@ -1,54 +1,95 @@
 package pipes
 
 import (
-  "syscall"
-  "io/ioutil"
+	"io/ioutil"
+	"syscall"
 )
 
 // By default WriteToPipe blocks execution until there's a reader.
-func WriteToPipe(filename string, msg []byte) error{
-  return ioutil.WriteFile(filename, msg, 0644)
+func WriteToPipe(filename string, msg []byte) error {
+	return ioutil.WriteFile(filename, msg, 0644)
 }
 
 // By default ReadFromPipe blocks execution until there's a writer.
-func ReadFromPipe(filename string)([]byte, error){
-  return ioutil.ReadFile(filename)
+func ReadFromPipe(filename string) ([]byte, error) {
+	return ioutil.ReadFile(filename)
 }
 
 // Write to Pipe, Block until content available, Read.
-func WriteBlockRead(filename string, msg []byte, resp chan []byte)(error){
-  err := WriteToPipe(filename, msg)
-  if err != nil{
-    return err
-  }
-  response, err := ReadFromPipe(filename)
-  if err != nil{
-    return err
-  }
-  resp <- response
-  return nil
+func WriteBlockRead(filename string, msg []byte, resp chan []byte) error {
+	err := WriteToPipe(filename, msg)
+	if err != nil {
+		return err
+	}
+	response, err := ReadFromPipe(filename)
+	if err != nil {
+		return err
+	}
+	resp <- response
+	return nil
 }
 
 // Write to Pipe, Block until content available, Read.
 // Insert a callback thing here.
-func ReadBlockWrite(filename string, com chan []byte)(error){
-  request, err := ReadFromPipe(filename)
-  if err != nil{
-    return err
-  }
-  // Communicate the request to listeners.
-  com <- request
-  // Block until other goroutines have created the response.
-  msg := <- com
-  err = WriteToPipe(filename, msg)
-  if err != nil{
-    return err
-  }
-  return nil
+func ReadBlockWrite(filename string, com chan []byte) error {
+	request, err := ReadFromPipe(filename)
+	if err != nil {
+		return err
+	}
+	// Communicate the request to listeners.
+	com <- request
+	// Block until other goroutines have created the response.
+	msg := <-com
+	err = WriteToPipe(filename, msg)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
+// Write to Pipe, Block until content available, Read.
+// Insert a callback thing here.
+func BetterReadBlockWrite(f string) (chan []byte, error) {
+	out := make(chan []byte)
+	go func() {
+		request, err := ReadFromPipe(f)
+		if err != nil {
+			// Do nothing for the moment.
+		}
+		// Communicate the request to listeners.
+		out <- request
+		response := <-out
+		// Block until other goroutines have created the response.
+		err = WriteToPipe(f, response)
+		if err != nil {
+			// Do nothing for the moment
+		}
+	}()
+	return out, nil
+}
+
+// Write to Pipe, Block until content available, Read.
+// Insert a callback thing here.
+func BetterWriteBlockRead(f string) (chan []byte, error) {
+	out := make(chan []byte)
+	go func() {
+		// Writes to pipe when a signal is available on the channel.
+		err := WriteToPipe(f, <-out)
+		if err != nil {
+			// Do nothing for the moment.
+		}
+		// Blocks until response available.
+		response, err := ReadFromPipe(f)
+		if err != nil {
+			// Do nothing for the moment.
+		}
+		// Put the response on the channel.
+		out <- response
+	}()
+	return out, nil
+}
 
 // Make a pipe.
-func MakeAPipe(name string){
-  syscall.Mknod(name, syscall.S_IFIFO|0666, 0)
+func MakeAPipe(name string) {
+	syscall.Mknod(name, syscall.S_IFIFO|0666, 0)
 }
--- pipes_test.go
+++ pipes_test.go
@@ -4,22 +4,67 @@ import (
 	"testing"
 )
 
-func TestReadBlockWrite(t *testing.T){
-  MakeAPipe("First")
-  go WriteToPipe("First", []byte("First request"))
+func TestReadBlockWrite(t *testing.T) {
+	MakeAPipe("First")
+	go WriteToPipe("First", []byte("First request"))
 
-  var comms = make(chan []byte)
-  go ReadBlockWrite ("First", comms)  
-  req := <- comms
-  if string(req) != "First request" {
-    t.Errorf("Want %v, got %v ", "First request", string(req))
-  }
-  comms <- []byte("Response")
-  resp, err := ReadFromPipe("First")
-  if err != nil {
-    t.Errorf("ReadFromPipe: %v", err)
-  }
-  if string(resp) != "Response" {
-    t.Errorf("Got %v, want %v ", string(resp), "Response")
-  } 
+	var comms = make(chan []byte)
+	go ReadBlockWrite("First", comms)
+	req := <-comms
+	if string(req) != "First request" {
+		t.Errorf("Want %v, got %v ", "First request", string(req))
+	}
+	comms <- []byte("Response")
+	resp, err := ReadFromPipe("First")
+	if err != nil {
+		t.Errorf("ReadFromPipe: %v", err)
+	}
+	if string(resp) != "Response" {
+		t.Errorf("Got %v, want %v ", string(resp), "Response")
+	}
 }
+
+func TestBetterReadBlockWrite(t *testing.T) {
+	MakeAPipe("First")
+	go WriteToPipe("First", []byte("First request"))
+
+	msg, _ := BetterReadBlockWrite("First")
+	req := <-msg
+	if string(req) != "First request" {
+		t.Errorf("Want %v, got %v ", "First request", string(req))
+	}
+	msg <- []byte("Response")
+	resp, err := ReadFromPipe("First")
+	if err != nil {
+		t.Errorf("ReadFromPipe: %v", err)
+	}
+	if string(resp) != "Response" {
+		t.Errorf("Got %v, want %v ", string(resp), "Response")
+	}
+}
+
+func TestBetterWriteBlockRead(t *testing.T) {
+	MakeAPipe("Second")
+	//defer *to implement a close function*
+	msg, _ := BetterWriteBlockRead("Second")
+	msg <- []byte("1. Request")
+	req, err := ReadFromPipe("Second")
+	if err != nil {
+		t.Errorf("ReadFromPipe: %v", err)
+	}
+	if string(req) != "1. Request" {
+		t.Errorf("Got %v, want %v", string(req), "1. Request")
+	}
+
+	go WriteToPipe("Second", []byte("2. Response"))
+	resp := <-msg
+	if string(resp) != "2. Response" {
+		t.Errorf("Got %v, want %v", string(resp), "2. Response")
+	}
+	//msg
+}
+
+//TODO:
+//1. Handle errors properly by a struct of stuff
+//2. Enable timeouts using the select idiom
+//3. Cleanup pipes after you're done by creating a close function.
